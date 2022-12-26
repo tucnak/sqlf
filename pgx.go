db8026fd48f68125308f4cc8fb0ffedb97e753a2
Ilya Kowalewski <ilya@hrest.org>
2022-12-26 18:44:18 +0200

    pgx support mk.3

--- pgx.go
+++ pgx.go
@@ -9,7 +9,7 @@ import (
 )
 
 type pgtag = pgconn.CommandTag
-type ctx context.Context
+type ctx = context.Context
 type pgx_ interface {
 	Exec(ctx ctx, sql string, args ...any) (pgtag, error)
 	Query(ctx ctx, sql string, args ...any) (pgx.Rows, error)
@@ -27,7 +27,7 @@ func (s *Stmt) Via(ctx context.Context, tx pgx_) *StmtPgx {
 	return &StmtPgx{s, ctx, tx, nil}
 }
 
-func (s *StmtPgx) Execf(args ...any) (pgtag, error) {
+func (s *StmtPgx) Exec(args ...any) (pgtag, error) {
 	return s.tx.Exec(s.ctx, s.Stmt.String(), args...)
 }
 
@@ -35,6 +35,13 @@ func (s *StmtPgx) Scan(args ...any) *StmtPgx {
 	return &StmtPgx{s.Stmt, s.ctx, s.tx, args}
 }
 func (s *StmtPgx) Row(dst ...any) error {
+	if len(dst) == 1 {
+		rows, err := s.tx.Query(s.ctx, s.Stmt.String(), s.args...)
+		if err != nil {
+			return err
+		}
+		return pgxscan.ScanOne(dst[0], rows)
+	}
 	row := s.tx.QueryRow(s.ctx, s.Stmt.String(), s.args...)
 	return row.Scan(dst...)
 }
