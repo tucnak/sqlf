62f1b663f0759dedb9132c5391e456724604a779
Ilya Kowalewski <ilya@hrest.org>
2023-01-10 16:29:46 +0200

    pgx support mk.4

--- pgx.go
+++ pgx.go
@@ -18,35 +18,24 @@ type pgx_ interface {
 
 type StmtPgx struct {
 	*Stmt
-	ctx  context.Context
-	tx   pgx_
-	args []any
+	ctx context.Context
+	tx  pgx_
 }
 
 func (s *Stmt) Via(ctx context.Context, tx pgx_) *StmtPgx {
-	return &StmtPgx{s, ctx, tx, nil}
+	return &StmtPgx{s, ctx, tx}
 }
 
-func (s *StmtPgx) Exec(args ...any) (pgtag, error) {
-	return s.tx.Exec(s.ctx, s.Stmt.String(), args...)
+func (s *StmtPgx) Exec() (pgtag, error) {
+	return s.tx.Exec(s.ctx, s.Stmt.String(), s.Stmt.Args()...)
 }
 
-func (s *StmtPgx) Scan(args ...any) *StmtPgx {
-	return &StmtPgx{s.Stmt, s.ctx, s.tx, args}
-}
 func (s *StmtPgx) Row(dst ...any) error {
-	if len(dst) == 1 {
-		rows, err := s.tx.Query(s.ctx, s.Stmt.String(), s.args...)
-		if err != nil {
-			return err
-		}
-		return pgxscan.ScanOne(dst[0], rows)
-	}
-	row := s.tx.QueryRow(s.ctx, s.Stmt.String(), s.args...)
+	row := s.tx.QueryRow(s.ctx, s.Stmt.String(), s.Stmt.Args()...)
 	return row.Scan(dst...)
 }
 func (s *StmtPgx) Rows(dst any) error {
-	rows, err := s.tx.Query(s.ctx, s.Stmt.String(), s.args...)
+	rows, err := s.tx.Query(s.ctx, s.Stmt.String(), s.Stmt.Args()...)
 	if err != nil {
 		return err
 	}
